064c13cdd614d5f5c333157c4408645e9285fcc0
Amine <aminedata1990@gmail.com>
2025-01-15 16:00:19 +0100

    book_system

---
+++ booksystem/book_system.js
@@ -0,0 +1,47 @@
+let books = []; // Assurez-vous d'avoir défini le tableau `books` pour stocker les livres
+
+function addBook() {
+    const bookName = document.getElementById('bookName').value;
+    const authorName = document.getElementById('authorName').value;
+    const bookDescription = document.getElementById('bookDescription').value;
+    const pagesNumber = parseInt(document.getElementById('pagesNumber').value);
+
+    // Vérifiez si tous les champs sont correctement remplis
+    if (bookName && authorName && bookDescription && !isNaN(pagesNumber)) {
+        const book = {
+            name: bookName,
+            authorName: authorName,
+            bookDescription: bookDescription,
+            pagesNumber: pagesNumber
+        };
+
+        // Ajouter le livre à la liste
+        books.push(book);
+        showbooks();
+        clearInputs();
+    } else {
+        alert('Please fill in all fields correctly.');
+    }
+}
+
+function showbooks() {
+    const booksDiv = books.map((book, index) => `
+        <div>
+            <h1>Book Number: ${index + 1}</h1>
+            <p><strong>Book Name: </strong>${book.name}</p>
+            <p><strong>Author Name: </strong>${book.authorName}</p>
+            <p><strong>Book Description: </strong>${book.bookDescription}</p>
+            <p><strong>No. of Pages: </strong>${book.pagesNumber} page(s)</p>
+        </div>
+    `);
+
+    // Afficher tous les livres dans la div avec l'ID "books"
+    document.getElementById('books').innerHTML = booksDiv.join('');
+}
+
+function clearInputs() {
+    document.getElementById('bookName').value = '';
+    document.getElementById('authorName').value = '';
+    document.getElementById('bookDescription').value = '';
+    document.getElementById('pagesNumber').value = '';
+}
